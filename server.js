91a5d7c12d6144efbc261a1cbeb8b283992b563b
Adrián Gómez <adgllorente@gmail.com>
2016-02-21 15:44:43 +0100

    Add Heroku port.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express'),
   app = express(),
   IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-  PORT = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+  PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8000;
 
 app.use('/', express.static(__dirname + '/static'));
 
