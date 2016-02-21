0b72beff2c58d78974511a4dc7d0ca6dae9d0f52
Adrián Gómez <adgllorente@gmail.com>
2016-02-21 16:00:02 +0100

    Add IP_ADDRESS

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 var express = require('express'),
   app = express(),
-  IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP,
   PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8000;
 
 app.use('/', express.static(__dirname + '/static'));
 
-app.listen(PORT, function () {
+app.listen(PORT, IP_ADDRESS, function () {
   console.log('Example app listening on port', PORT);
 });
